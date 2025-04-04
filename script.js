8aa874dd9b2a017ddd67689b72fe5e04f52d2d91
Sandeepa Sineth <165464096+Sandeepasineth@users.noreply.github.com>
2025-04-05 01:49:42 +0530

    script.js

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ document.getElementById("contactForm").addEventListener("submit", function(event
         email: document.getElementById("email").value,
         subject: document.getElementById("subject").value,
         message: document.getElementById("message").value,
-   
+        time: new Date().toLocaleString() 
     };
 
     // Send email via EmailJS
